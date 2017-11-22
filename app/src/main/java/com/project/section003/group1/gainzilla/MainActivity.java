5faea87a1453b45b402670b22ec353b319c53898
DESKTOP-SOGRI79\muham <zabidin@gmu.edu>
2017-11-22 18:55:28 -0500

    adding more to home page

--- app/src/main/java/com/project/section003/group1/gainzilla/MainActivity.java
+++ app/src/main/java/com/project/section003/group1/gainzilla/MainActivity.java
@@ -1,7 +1,14 @@
 package com.project.section003.group1.gainzilla;
 
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button start = (Button) findViewById(R.id.Start);
+        Button addworkout = (Button) findViewById(R.id.add);
+        TextView yLabel = (TextView)findViewById(R.id.yLabel);
+        TextView mLabel = (TextView)findViewById(R.id.mLabel);
+        TextView dLabel = (TextView)findViewById(R.id.dLabel);
+        TextView eLabel = (TextView)findViewById(R.id.eLabel);
+
+
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MMMM/d/E", Locale.US); // Set your locale!
+        String strDate = sdf.format(cal.getTime());
+
+        String[] values = strDate.split("/",0);
+
+
+        yLabel.setText(values[0]);
+        mLabel.setText(values[1]);
+        dLabel.setText(values[2]);
+        eLabel.setText(values[3]);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                setContentView(R.layout.workout);
+            }
+        });
+        addworkout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                setContentView(R.layout.add);
+            }
+        });
+
+
+
+
     }
 }

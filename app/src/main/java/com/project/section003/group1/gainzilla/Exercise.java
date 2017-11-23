894fb2b22774cd9f377b6f18e583b049ea86401f
B27 <calderonboris@gmail.com>
2017-11-22 22:50:10 -0500

    - Adjusted Exercise.java

--- app/src/main/java/com/project/section003/group1/gainzilla/Exercise.java
+++ app/src/main/java/com/project/section003/group1/gainzilla/Exercise.java
@@ -8,72 +8,75 @@ import android.support.design.widget.TextInputLayout;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 
 
 public class Exercise extends AppCompatActivity {
 
-    Button mButton;
-    EditText mEdit;
-    TextView mText;
-    //
-    EditText mEdit2;
-    TextView mText2;
-    //
-    EditText mEdit3;
-    TextView mText3;
-    //
-    EditText mEdit4;
-    TextView mText4;
-
+/*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
 
     }
+*/
+    EditText editText;
+    EditText editText2;
+    EditText editText3;
+    EditText editText4;
 
-    public void buttonClicked(View v){
-        Button mButton = (Button) findViewById(R.id.createButton);
+    Button addButton;
+    ListView listView;
+    ArrayList<String> listItems;
+    ArrayAdapter<String> adapter;
 
-        mButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                mEdit   = (EditText)findViewById(R.id.textInputEditText);
-                String mEdit = ((EditText) findViewById(R.id.textInputEditText)).getText().toString();
-                mText = (TextView)findViewById(R.id.textInputEditText);
-                mText.setText("qqqqq");
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise);
+
+        editText = (EditText) findViewById(R.id.textInputEditText);
+        editText2 = (EditText) findViewById(R.id.textInputEditText2);
+        editText3 = (EditText) findViewById(R.id.textInputEditText3);
+        editText4 = (EditText) findViewById(R.id.textInputEditText4);
+
+
+        addButton = (Button) findViewById(R.id.createButton);
+        listView = (ListView) findViewById(R.id.exList);
+
+        listItems = new ArrayList<>();
+        listItems.add(editText);
+
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, listItems);
+        listView.setAdapter(adapter);
+        addButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                listItems.add(editText.getText().toString());
+                adapter.notifyDataSetChanged();
             }
         });
-
-        mButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                mEdit2   = (EditText)findViewById(R.id.textInputEditText3);
-                String mEdit = ((EditText) findViewById(R.id.textInputEditText3)).getText().toString();
-                mText2 = (TextView)findViewById(R.id.textInputEditText3);
-                mText2.setText("qqqqq");
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> a, View v, int position,
+                                    long id) {
+                Toast.makeText(Exercise.this, "Clicked", Toast.LENGTH_LONG)
+                        .show();
             }
         });
-
-        mButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                mEdit3   = (EditText)findViewById(R.id.textInputEditText2);
-                String mEdit = ((EditText) findViewById(R.id.textInputEditText2)).getText().toString();
-                mText3 = (TextView)findViewById(R.id.textInputEditText2);
-                mText3.setText("qqqqq");
-            }
-        });
-
-        mButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                mEdit4   = (EditText)findViewById(R.id.textInputEditText4);
-                String mEdit = ((EditText) findViewById(R.id.textInputEditText4)).getText().toString();
-                mText4 = (TextView)findViewById(R.id.textInputEditText4);
-                mText4.setText("qqqqq");
-            }
-        });
-
     }
-    }
+}
+
+
+

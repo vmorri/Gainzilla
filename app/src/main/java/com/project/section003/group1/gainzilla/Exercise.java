b5f61b8fb806c77c850d4e333fa88ef56dcc9d3f
B27 <calderonboris@gmail.com>
2017-11-22 19:26:20 -0500

    - Added Exercise under Java file - Added activity_exercise.xml under layout - Added Exercise related name ids under values

---
+++ app/src/main/java/com/project/section003/group1/gainzilla/Exercise.java
@@ -0,0 +1,13 @@
+package com.project.section003.group1.gainzilla;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Exercise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise);
+    }
+}

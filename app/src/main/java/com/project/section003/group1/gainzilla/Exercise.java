4c52aad9d08d6a285871a52afabc3932590111b0
Michael Morris <vinh.morris@gmail.com>
2017-11-22 19:55:23 -0500

    Testing .gitignore

--- app/src/main/java/com/project/section003/group1/gainzilla/Exercise.java
+++ app/src/main/java/com/project/section003/group1/gainzilla/Exercise.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 
 public class Exercise extends AppCompatActivity {
 
+
+    // I AM LOVING THIS APP
+    // GKAJSKJBLKJWDKJNDWLKjn
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

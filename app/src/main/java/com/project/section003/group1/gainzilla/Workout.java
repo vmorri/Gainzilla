3d2f89815afa264d2682ba63f09d81da33184e82
B27 <calderonboris@gmail.com>
2017-11-26 15:37:19 -0500

    - Reverted changes on Workout.java to original. I didn't realize WorkoutActivity.java had implemented what I had added to Workout.Java

--- app/src/main/java/com/project/section003/group1/gainzilla/Workout.java
+++ app/src/main/java/com/project/section003/group1/gainzilla/Workout.java
@@ -1,9 +1,5 @@
 package com.project.section003.group1.gainzilla;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-
 import java.util.ArrayList;
 
 
@@ -20,31 +16,11 @@ import java.util.ArrayList;
 * WorkoutList class creates objects that contain a list of workouts. Only one WorkoutList object
 * should ever exist.
 * */
-public class Workout extends AppCompatActivity{
+public class Workout{
     public String name;
     public int Date;
     public ArrayList<Exercise> ex;
     public int rest;
-    Intent intent;
-    Bundle bundle;
-    String pass_exercise_from_exercise;
-
-
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        /** get the intent */
-        intent = getIntent();
-
-        /** get the bundle from intent*/
-        bundle = intent.getExtras();
-        /** retrieve the string extra passed */
-        pass_exercise_from_exercise = bundle.getString("exercise_to_pass");
-
-        /** retrieve the stringarray extra passed */
-        // String[] arrRecd = b.getStringArray("array");
-        // textView.setText(a + ":val:" + arrRecd[1]);
-    }
 
     //Constructor for Workout
     public Workout(String name){

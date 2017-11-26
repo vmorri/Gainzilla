a82a3531642d0a23c6addf54ac2a4204d5d846a9
B27 <calderonboris@gmail.com>
2017-11-26 15:26:22 -0500

    - Added intent that sends exercise (string) per button click (on Exercise.java) to Workout.java (string pass_exercise_from_exercise).

--- app/src/main/java/com/project/section003/group1/gainzilla/Exercise.java
+++ app/src/main/java/com/project/section003/group1/gainzilla/Exercise.java
@@ -1,5 +1,6 @@
 package com.project.section003.group1.gainzilla;
 
+import android.content.Intent;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -14,12 +15,17 @@ import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
 public class Exercise extends AppCompatActivity {
     ArrayAdapter<String> adapter;
     EditText editText;
     EditText editText2;
     EditText editText3;
     EditText editText4;
+    String newItem;
+    Bundle bundle;
+    Intent intent;
 
     ArrayList<String> itemList;
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,23 +41,38 @@ public class Exercise extends AppCompatActivity {
         editText=(EditText)findViewById(R.id.txtInput);
         editText2=(EditText)findViewById(R.id.txtInput2);
         editText3=(EditText)findViewById(R.id.txtInput3);
-        editText4=(EditText)findViewById(R.id.txtInput3);
+        editText4=(EditText)findViewById(R.id.txtInput4);
 
 
         Button btAdd=(Button)findViewById(R.id.btAdd);
         btAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String newItem=("Name:" + editText.getText().toString() + "     Weight:" + editText2.getText().toString() + "        Sets:" + editText3.getText().toString() + "         Reps:" + editText4.getText().toString() + "");
-
+                newItem=("Name:" + editText.getText().toString() + "     Weight:" + editText2.getText().toString() + "        Sets:" + editText3.getText().toString() + "         Reps:" + editText4.getText().toString() + "");
                 // add new item to arraylist
                 itemList.add(newItem);
                 // notify listview of data changed
                 adapter.notifyDataSetChanged();
-            }
 
+                /** getting the value of edit text entered by user */
+                String exercise_to_pass = newItem;
+                Intent intent = new Intent(Exercise.this, Workout.class);
+
+                /** setting the textVal in intentExtra */
+                /** passing string */
+                bundle = new Bundle();
+                bundle.putString("exercise_to_pass", exercise_to_pass);
+
+                /** passing string array*/
+                // bundle.putStringArray("array", arr);
+                // intent.putExtras(bundle);
+
+                /** start Activity2 */
+                startActivity(intent);
+            }
         });
 
     }
+
 
 }
--- app/src/main/java/com/project/section003/group1/gainzilla/Workout.java
+++ app/src/main/java/com/project/section003/group1/gainzilla/Workout.java
@@ -1,12 +1,36 @@
 package com.project.section003.group1.gainzilla;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 import java.util.ArrayList;
 
-public class Workout {
+public class Workout extends AppCompatActivity {
     public String name;
     public int Date;
     public ArrayList<Exercise> ex;
     public int rest;
+    Intent intent;
+    Bundle bundle;
+    String pass_exercise_from_exercise;
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /** get the intent */
+        intent = getIntent();
+
+        /** get the bundle from intent*/
+        bundle = intent.getExtras();
+        /** retrieve the string extra passed */
+        pass_exercise_from_exercise = bundle.getString("exercise_to_pass");
+
+        /** retrieve the stringarray extra passed */
+        // String[] arrRecd = b.getStringArray("array");
+        // textView.setText(a + ":val:" + arrRecd[1]);
+    }
 
     public Workout(String name){
         this.name = name;
@@ -34,4 +58,6 @@ public class Workout {
     private void save(){
 
     }
+
+
 }

b0555c986037be8a79c3e5d66e3ba32377c005d8
John <hunt2882@yahoo.com>
2017-11-26 22:45:55 -0500

    display of exercises from start workout

--- app/src/main/java/com/project/section003/group1/gainzilla/WorkoutDisplayActivity.java
+++ app/src/main/java/com/project/section003/group1/gainzilla/WorkoutDisplayActivity.java
@@ -1,32 +1,84 @@
 package com.project.section003.group1.gainzilla;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
 public class WorkoutDisplayActivity extends AppCompatActivity {
-    ArrayAdapter<Exercise> adapter;
-    ArrayList<Exercise> itemList;
+    private ListView lv;
+    final Context context = this;
+    private Button button;
+    private EditText result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_workout_disp);
+        setContentView(R.layout.activity_workout_display);
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(WorkoutActivity.EXTRA_MESSAGE);
-        int workoutNum = Integer.parseInt(message);
-        String[] items={""};
-        //itemList=new ArrayList<String>(Arrays.asList(items));
-        adapter=new ArrayAdapter<Exercise>(this, android.R.layout.simple_list_item_1, WorkoutList.listworkouts.get(workoutNum -1).ex);
+        final int workoutNum = Integer.parseInt(message);
 
-        ListView LV=(ListView)findViewById(R.id.exerList);
-        LV.setAdapter(adapter);
+        lv = (ListView) findViewById(R.id.exerList3);
+        List<Exercise> your_array_list = WorkoutList.listworkouts.get(workoutNum -1).ex;
+        ArrayAdapter<Exercise> arrayAdapter = new ArrayAdapter<Exercise>(
+                this,
+                android.R.layout.simple_list_item_1,
+                your_array_list );
+        lv.setAdapter(arrayAdapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                LayoutInflater li = LayoutInflater.from(context);
+                View promptsView = li.inflate(R.layout.weight_prompt, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context);
+
+                alertDialogBuilder.setView(promptsView);
+
+                final EditText userInput = (EditText) promptsView
+                        .findViewById(R.id.editTextDialogUserInput);
+
+                // set dialog message
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        // get user input and set it to result
+                                        // edit weight
+                                        WorkoutList.listworkouts.get(workoutNum - 1).ex.get(position).weight = (EditText) userInput.getText();
+                                    }
+                                })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+            }
+        });
+
     }
 
 }
